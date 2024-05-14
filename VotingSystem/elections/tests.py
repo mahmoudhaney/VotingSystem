bfbf8b7080dc148369fc8fb23517b047b4bd990b
Mahmoud Haney <mahmoudhaney25@gmail.com>
2024-05-14 23:19:24 +0300

    Test Cases for the Voting process

--- VotingSystem/elections/tests.py
+++ VotingSystem/elections/tests.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase 
 from django.urls import reverse
-from users.tests import create_admin_user, authenticate_user, generate_test_image
+from users.tests import create_admin_user, authenticate_user, generate_test_image, create_basic_user
 from rest_framework import status
 from .models import Election
 
@@ -33,7 +33,7 @@ def create_election(instance, url, data, headers):
     response = instance.client.post(url, data, **headers)
     return response
 
-class ElectionAPITestCase(APITestCase):
+class ElectionListCreateViewTestCase(APITestCase):
     def setUp(self):
         self.url = create_list_election_url()
         self.admin_user = create_admin_user()
@@ -154,3 +154,118 @@ class ElectionCandidatesViewTestCase(APITestCase):
         response = self.client.get(f'{self.url}{election_response.data["uuid"]}/candidates/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+class VoteCreateViewTestCase(APITestCase):
+    def setUp(self):
+        # Create elections
+        self.admin_user = create_admin_user()
+        refresh_token, access_token = authenticate_user(self.admin_user)
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        election_data = {
+        'name': 'Test Election 2024',
+        'description': 'Test Description',
+        'start_date': '2024-04-26T00:00:00Z',
+        'end_date': '2024-04-28T00:00:00Z',
+        }
+        self.election1_response = self.client.post(reverse('elections:elections-list-create'), election_data, **headers)
+        self.election1_uuid = self.election1_response.data['uuid']
+        self.election2_response = self.client.post(reverse('elections:elections-list-create'), election_data, **headers)
+        self.election2_uuid = self.election2_response.data['uuid']
+
+        # Create a candidates
+        self.image_file = generate_test_image()
+        candidate_data = {
+            'name': 'Test Candidate One',
+            'bio': 'Test Bio',
+            'photo': self.image_file,
+            'election_uuid': self.election1_uuid
+        }
+        self.candidate1_response = self.client.post(reverse('users:candidate_create'), candidate_data, **headers)
+        self.candidate1_uuid = self.candidate1_response.data['uuid']
+
+        candidate_data['name'] = 'Test Candidate Two'
+        candidate_data['photo'] = generate_test_image()
+        candidate_data['election_uuid'] = self.election2_uuid
+        self.candidate2_response = self.client.post(reverse('users:candidate_create'), candidate_data, **headers)
+        self.candidate2_uuid = self.candidate2_response.data['uuid']
+
+        self.url = reverse('elections:vote')
+        self.user = create_basic_user()
+        self.refresh_token, self.access_token = authenticate_user(self.user)
+        self.headers = HTTP_AUTHORIZATION_HEADER(self.access_token)
+        self.data = {
+            'election_uuid': self.election1_uuid,
+            'candidate_uuid': self.candidate1_uuid,
+        }
+
+    def test_create_vote_success(self):
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('election_uuid', response.data)
+        self.assertIn('candidate_uuid', response.data)
+
+    def test_create_vote_with_missing_data(self):
+        response = self.client.post(self.url, {}, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('election_uuid', response.data)
+        self.assertIn('candidate_uuid', response.data)
+
+    def test_create_vote_candidate_not_in_election(self):
+        self.data['candidate_uuid'] = self.candidate2_uuid
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('candidate', response.data)
+        self.assertEqual(response.data['candidate'][0], 'This candidate not found for this election.')
+
+    def test_create_vote_with_invalid_election_uuid(self):
+        self.data['election_uuid'] = 'invalid_uuid'
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('election_uuid', response.data)
+
+    def test_create_vote_with_invalid_candidate_uuid(self):
+        self.data['candidate_uuid'] = 'invalid_uuid'
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('candidate_uuid', response.data)
+
+    def test_create_vote_election_not_found(self):
+        self.data['election_uuid'] = '00000000-0000-0000-0000-000000000000'
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_vote_candidate_not_found(self):
+        self.data['candidate_uuid'] = '00000000-0000-0000-0000-000000000000'
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_vote_with_invalid_election(self):
+        # Deactivate the election by setting is_active to False
+        election = Election.objects.get(uuid=self.election1_uuid)
+        election.is_active = False
+        election.save()
+        # Attempt to vote in the election
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('election_uuid', response.data)
+
+    def test_create_vote_with_existing_vote(self):
+        self.client.post(self.url, self.data, **self.headers)
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('voter', response.data)
+
+    def test_create_vote_without_authentication(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_vote_with_wrong_authentication(self):
+        headers = {'HTTP_AUTHORIZATION': f'Bearer wrong_token_here'}
+        response = self.client.post(self.url, self.data, **headers)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_vote_with_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        response = self.client.post(self.url, self.data, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
